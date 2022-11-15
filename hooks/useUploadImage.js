c20b777fb14b39734aa5ac13702a5831f61173a0
Nico Ignacio <norbertoignaciojr@gmail.com>
2022-11-15 22:06:36 +0800

    fix: added img url for display

--- hooks/useUploadImage.js
+++ hooks/useUploadImage.js
@@ -1,6 +1,6 @@
 import { Alert } from 'react-native';
 import { useState } from 'react';
-import { ref, uploadBytes } from 'firebase/storage';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import * as ImagePicker from 'expo-image-picker';
 
 import { storage } from 'fitra/firebase.config';
@@ -60,14 +60,17 @@ export default function useUploadImage(id, filepath, metadata = {}) {
         // CREATE AN ID FOR THE COMMENT ID
         const fileId = id;
         const storageRef = ref(storage, filepath + fileId);
+        let fileExtension = image.uri.substring(image.uri.lastIndexOf('.') + 1);
 
         try {
-            const snapshot = await uploadBytes(storageRef, fileBlob, metadata);
+            const snapshot = await uploadBytes(storageRef, fileBlob, { ...metadata, fileExtension });
+            const imgUrl = await getDownloadURL(snapshot.ref);
             console.log("UPLOADED");
-            Alert.alert("Upload Completed", "The image upload was successful.");
             setImage(null);
             setIsUploading(false);
             setFilename("");
+            Alert.alert("Upload Completed", "The image upload was successful.");
+            return imgUrl;
         } catch (err) {
             console.log(err);
             console.log("UPLOAD FAILED");
