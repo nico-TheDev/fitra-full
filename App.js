f187ec455138abf705f968edaccf2a5bc11908a7
Jian Karlo Parcon <jiankarlo.xurpas@gmail.com>
2022-11-17 14:14:49 +0800

    test: initial commit

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import { AuthProvider } from "fitra/contexts/AuthContext";
 export default function App() {
     const [isAppReady, setIsAppReady] = useState(false);
 
+
     useEffect(() => {
         async function prepare() {
             try {
