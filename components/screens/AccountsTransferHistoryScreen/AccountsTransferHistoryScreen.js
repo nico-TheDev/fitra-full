a0e278d6cd13fc641323d539b4d86b2f00da6b42
xcboi2k <darkspikyshark@gmail.com>
2022-12-20 17:00:49 +0800

    feat: sorting by time, ongoing debugging

--- components/screens/AccountsTransferHistoryScreen/AccountsTransferHistoryScreen.js
+++ components/screens/AccountsTransferHistoryScreen/AccountsTransferHistoryScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // LOCAL IMPORTS
 
 import ScreenHeader from "components/ScreenHeader";
@@ -7,14 +7,17 @@ import AccountPanelItem from "components/AccountPanelItem";
 import colors from "assets/themes/colors";
 import { ICON_NAMES } from "constants/constant";
 import { transferHistoryLogData } from "fitra/SampleData";
+import convertTimestamp from "util/convertTimestamp";
+import formatDate from "util/formatDate";
+
 import {
     SectionHeader,
     TransferHistoryContainer,
     TransferSectionList,
 } from "./styles";
 
-import useTransferStore from "hooks/useTransferStore";
 import useAuthStore from "hooks/useAuthStore";
+import useTransferStore from "hooks/useTransferStore";
 import useTransferListener from "hooks/useTransferListener";
 
 const AccountsTransferHistoryScreen = ({ navigation }) => {
@@ -29,12 +32,84 @@ const AccountsTransferHistoryScreen = ({ navigation }) => {
     const user = useAuthStore(state => state.user);
     const [userTransfers] = useTransferListener(user.user_id);
 
+    const [historyData, setHistoryData] = useState([]);
+    const [filterValue, setFilterValue] = useState("year");
+
     const DATA = [
         {
             title: "Transfers",
             data: userTransfers
         },
     ];
+
+    useEffect(() => {
+        if (filterValue === "day") {
+            const sortedByDate = userTransfers.sort((a, b) => a.created_at.seconds > b.created_at.seconds);
+            // CREATE UNIQUE DAYS ARRAY
+            const uniqueDays = [];
+            const finalDayData = [];
+
+            // GET THE AVAILABLE DATES
+            sortedByDate.forEach(item => {
+                const currentDate = formatDate(convertTimestamp(item.created_at));
+                if (!uniqueDays.includes(currentDate)) {
+                    uniqueDays.push(currentDate);
+                }
+            });
+
+            // ADD THE DATES TO THE FINAL DATA
+            uniqueDays.forEach(date => {
+                const textDate = formatDate(new Date(date), true);
+                finalDayData.push({ title: textDate, data: [], date });
+            });
+
+            userTransfers.forEach(transfers => {
+                const currentDate = formatDate(convertTimestamp(transfers.created_at));
+                const targetIndex = finalDayData.findIndex(item => item.date === currentDate);
+
+                finalDayData[targetIndex].data.push(transfers);
+            });
+            setHistoryData(finalDayData);
+            console.log(finalDayData);
+        }
+
+        else if (filterValue === "month") {
+            const sortedByDate = userTransfers.sort((a, b) => a.created_at.seconds > b.created_at.seconds);
+            const finalMonthData = [];
+            const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            const uniqueMonths = [];
+
+            // GET THE AVAILABLE MONTHS
+            sortedByDate.forEach(item => {
+                const currentDate = formatDate(convertTimestamp(item.created_at));
+
+                const currentMonth = Number(currentDate.split("/")[0]) - 1;
+                if (!uniqueMonths.includes(monthNames[currentMonth])) {
+                    uniqueMonths.push(monthNames[currentMonth]);
+                }
+            });
+
+            // ADD THE MONTHS TO THE FINAL DATA
+            uniqueMonths.forEach(month => {
+                finalMonthData.push({ title: month, data: [], });
+            });
+
+            userTransfers.forEach(transfers => {
+                const currentDate = formatDate(convertTimestamp(transfers.created_at));
+                const currentMonth = monthNames[currentDate.split("/")[0] - 1];
+                const targetIndex = finalMonthData.findIndex(item => item.title === currentMonth);
+
+                finalMonthData[targetIndex].data.push(transfers);
+            });
+            setHistoryData(finalMonthData);
+
+        } else if (filterValue === "year") {
+            setHistoryData([{
+                title: "2022",
+                data: userTransfers
+            }]);
+        }
+    }, [filterValue])
 
     const handleNavigate = (id) =>
         navigation.navigate("Accounts", {
@@ -65,7 +140,7 @@ const AccountsTransferHistoryScreen = ({ navigation }) => {
     return (
         <TransferHistoryContainer>
             <ScreenHeader title="Transfer History" />
-            <FilterInput items={items} setItems={setItems} />
+            <FilterInput items={items} setItems={setItems} setValue={setFilterValue} value={filterValue}/>
             <TransferSectionList
                 sections={DATA}
                 keyExtractor={(item, index) => item + index}
